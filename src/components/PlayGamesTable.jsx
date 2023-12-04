862a68803b7ebf3b227360aaef863d2a00810068
Carolina <49445004+carocancode@users.noreply.github.com>
2023-12-03 22:42:16 -0600

    Update PlayGamesTable.jsx

--- src/components/PlayGamesTable.jsx
+++ src/components/PlayGamesTable.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import PlayGamesRow from './PlayGamesRow';
 
-function PlayGamesTable({ games, onPlay}) {
+
+function PlayGamesTable({ games, onPlay }) {
     return (
-        <table id='games-table'>
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Title</th>
-                    <th>Date Created</th>
-                    <th>Date Modified</th>
-                </tr>
-            </thead>
-            <tbody>
-                {games.map(game => <PlayGamesRow key={game._id} game={game} onPlay={onPlay} />)}
-            </tbody>
-        </table>
+        <div className="table-container">
+            <table id='games-table'>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Title</th>
+                        <th>Date Created</th>
+                        <th>Date Modified</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {games.map(game => <PlayGamesRow key={game._id} game={game} onPlay={onPlay} />)}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
