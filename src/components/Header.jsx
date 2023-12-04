172d42f03ff279be6cb112c96b9cbfdb6c3e9515
Carolina <49445004+carocancode@users.noreply.github.com>
2023-12-03 22:05:07 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,45 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthBoxLoggedOut from './AuthBoxLoggedOut.jsx';
 import AuthBoxLoggedIn from './AuthBoxLoggedIn.jsx';
 
 function Header() {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('user')))
+  const [user, setUser] = useState(() => JSON.parse(localStorage.getItem('user')) || null);
+  const [emailDisplayed, setEmailDisplayed] = useState(false);
 
   useEffect(() => {
     // Refresh the component whenever the localStorage user data changes.
     const handleStorageChange = () => {
-      setUser(JSON.parse(localStorage.getItem('user')))
-    }
+      setUser(JSON.parse(localStorage.getItem('user')));
+    };
 
-    window.addEventListener('storage', handleStorageChange)
+    window.addEventListener('storage', handleStorageChange);
 
     return () => {
-      window.removeEventListener('storage', handleStorageChange)
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
+
+  const displayEmail = () => {
+    if (user && user.email && !emailDisplayed) {
+      setEmailDisplayed(true);
+      return <p>{user.email}</p>;
     }
-  }, [])
+    return null;
+  };
 
   return (
-    <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+    <header style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h1>Text Adventure Game For Education</h1>
-      {user && user.email ? <AuthBoxLoggedIn user={user} setUser={setUser} /> : <AuthBoxLoggedOut />} 
+      
+      {user && user.email ? (
+        <div class = "login">
+          
+          <AuthBoxLoggedIn user={user} setUser={setUser} />
+          {displayEmail()}
+        </div>
+      ) : (
+        <AuthBoxLoggedOut />
+      )}
     </header>
   );
 }
