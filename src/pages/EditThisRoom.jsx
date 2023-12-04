f79a330e5541d3b10427533e46377b9b7620c97d
Merlin Mallory <merlin.mallory.coder@gmail.com>
2023-12-04 19:47:57 +0900

    Attempted to add autosave after add room in GameEdit, but its still bugged

--- src/pages/EditThisRoom.jsx
+++ src/pages/EditThisRoom.jsx
@@ -5,6 +5,7 @@ function EditThisRoom() {
     const { gameId, roomIndex } = useParams();
     const navigate = useNavigate();
     const [game, setGame] = useState(null);
+    const [playerChoices, setPlayerChoices] = useState([]);
     const user = JSON.parse(localStorage.getItem('user'));
 
     useEffect(() => {
@@ -13,6 +14,13 @@ function EditThisRoom() {
                 const response = await fetch(`https://467-capstone-tag-production-bd6a.up.railway.app/games/details/${gameId}`);
                 const data = await response.json();
                 setGame(data);
+
+                if (data && data.game && data.game.rooms && data.game.rooms[roomIndex]) {
+                    const existingChoices = data.game.rooms[roomIndex].exits.map(exit => {
+                        return [exit[0] || '', parseInt(exit[1]) || 0];
+                    });
+                    setPlayerChoices(existingChoices);
+                }
             } catch (error) {
                 console.error('Error fetching room data...', error);
             }
@@ -21,34 +29,49 @@ function EditThisRoom() {
         fetchGame();
     }, [gameId, roomIndex]);
 
+    const addPlayerChoice = () => {
+        setPlayerChoices([...playerChoices, ['', 0]]);
+    };
+
+    const handlePlayerChoiceChange = (index, field, value) => {
+        const updatedChoices = [...playerChoices];
+        updatedChoices[index] = field === 'promptText' ? [value, updatedChoices[index][1]] : [updatedChoices[index][0], parseInt(value)];
+        setPlayerChoices(updatedChoices);
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (!user) {
             alert('You must be logged in to edit a game.');
             return;
         }
-    
+
+        const updatedRoom = {
+            ...game.game.rooms[roomIndex],
+            name: game.game.rooms[roomIndex].name,
+            description: game.game.rooms[roomIndex].description,
+            exits: playerChoices
+        };
+
         const updatedGame = { ...game };
-    
+        updatedGame.game.rooms[roomIndex] = updatedRoom;
+
         const updateResponse = await fetch(`https://467-capstone-tag-production-bd6a.up.railway.app/games/${game._id}`, {
             method: 'PATCH',
             body: JSON.stringify(updatedGame),
             headers: { 'Content-Type': 'application/json' }
         });
+
         if (!updateResponse.ok) {
             throw new Error('Failed to update data in the database');
         }
         console.log('Game successfully updated in the database');
     };
-    
-
 
     const handleInputChange = (field, value) => {
-        setGame(prevGame => {
-            const updatedRooms = [...prevGame.game.rooms];
-            updatedRooms[roomIndex] = {...updatedRooms[roomIndex], [field]: value};
-            return {...prevGame, game: {...prevGame.game, rooms: updatedRooms}};
-        });
+        const updatedRooms = [...game.game.rooms];
+        updatedRooms[roomIndex] = { ...updatedRooms[roomIndex], [field]: value };
+        setGame({ ...game, game: { ...game.game, rooms: updatedRooms } });
     };
 
     if (!game) return <div>Loading...</div>;
@@ -61,43 +84,48 @@ function EditThisRoom() {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label>Name:</label>
-                    <input 
-                        type="text" 
-                        value={currentRoom.name} 
-                        onChange={(e) => handleInputChange('name', e.target.value)} 
-                    />
+                    <input type="text" value={currentRoom.name} onChange={(e) => handleInputChange('name', e.target.value)} />
                 </div>
                 <div>
                     <label>Description:</label>
-                    <textarea 
-                        value={currentRoom.description} 
-                        onChange={(e) => handleInputChange('description', e.target.value)} 
-                    />
+                    <textarea value={currentRoom.description} onChange={(e) => handleInputChange('description', e.target.value)} />
                 </div>
                 <div>
-                    <label>First Visit:</label>
-                    <textarea 
-                        value={currentRoom.first_visit} 
-                        onChange={(e) => handleInputChange('first_visit', e.target.value)} 
-                    />
+                    <label>Player Choices:</label>
+                    <button type="button" onClick={addPlayerChoice}>Add Player Choice</button>
+                    {playerChoices.length === 0 ? (
+                        <p>No player choices added yet...</p>
+                    ) : (
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Prompt Text</th>
+                                    <th>Room Link</th>
+                                    <th>Link Name</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {playerChoices.map((choice, index) => (
+                                    <tr key={index}>
+                                        <td>
+                                            <input type="text" value={choice[0]} onChange={(e) => handlePlayerChoiceChange(index, 'promptText', e.target.value)} />
+                                        </td>
+                                        <td>
+                                            <select value={choice[1]} onChange={(e) => handlePlayerChoiceChange(index, 'roomLink', e.target.value)}>
+                                                {game.game.rooms.map((room, idx) => (
+                                                    <option key={idx} value={idx}>{idx}</option>
+                                                ))}
+                                            </select>
+                                        </td>
+                                        <td>
+                                            {game.game.rooms[choice[1]]?.name}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    )}
                 </div>
-                <div>
-                    <label>Items:</label>
-                    <input 
-                        type="text" 
-                        value={currentRoom.Items} 
-                        onChange={(e) => handleInputChange('Items', e.target.value)} 
-                    />
-                </div>
-                <div>
-                    <label>Exits:</label>
-                    <input 
-                        type="text" 
-                        value={currentRoom.Exits} 
-                        onChange={(e) => handleInputChange('Exits', e.target.value)} 
-                    />
-                </div>
-                
                 <button type="submit">Save Changes</button>
             </form>
             <button onClick={() => navigate(`/edit-game/${gameId}`)}>Back to Game Edit</button>
